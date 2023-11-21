c393d08afaf937791484b6f68e87dac45c28756f
Sharapatov Nikita <neffshar@gmail.com>
2023-11-21 16:37:10 +0300

    Исправил ошибки, дописал отношения, почистил не нужные файлы

--- prof_test_kubsau/database.py
+++ prof_test_kubsau/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-import datetime as dt
+from sqlalchemy.orm import validates, relationship, backref
+import datetime, re
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./sql_app.db'
 
@@ -10,7 +11,6 @@ engine = create_engine(
 )
 
 Base = declarative_base()
-today = dt.datetime.now()
 
 
 class Result(Base):
@@ -20,8 +20,16 @@ class Result(Base):
     surname = Column(String)
     name = Column(String)
     patronymic = Column(String)
-    phone_number = Column(Integer)
-    dt_created = Column(DateTime, default=today.isoformat())
+    phone_number = Column(String)
+    dt_created = Column(DateTime, default=datetime.datetime.utcnow().isoformat())
+
+    @validates('phone_number')
+    def validate_phone(self, key, phone_number):
+        if not re.match(r'79\d{9}', phone_number):
+            raise ValueError('Phone number should match the pattern "79#########"')
+        return phone_number
+
+    result_faculty = relationship('ResultFaculty', backref='result')
 
 
 class ResultFaculty(Base):
@@ -32,6 +40,9 @@ class ResultFaculty(Base):
     faculty_id = Column(Integer, ForeignKey('faculty.id'))
     compliance = Column(Integer)
 
+    result = relationship('Result', backref='result_faculty')
+    faculty = relationship('Faculty', backref='result_faculty')
+
 
 class AnswerFaculty(Base):
     __tablename__ = 'answer_faculty'
@@ -41,6 +52,9 @@ class AnswerFaculty(Base):
     faculty_id = Column(Integer, ForeignKey('faculty.id'))
     score = Column(Integer)
 
+    answer = relationship('Answer', backref='answer_faculty')
+    faculty = relationship('Faculty', backref='answer_faculty')
+
 
 class Answer(Base):
     __tablename__ = 'answer'
@@ -49,12 +63,16 @@ class Answer(Base):
     question_id = Column(Integer, ForeignKey('quest.id'))
     text = Column(String(200))
 
+    quest = relationship('Quest', backref='answer')
+
 
 class Quest(Base):
     __tablename__ = 'quest'
 
     id = Column(Integer, primary_key=True, index=True)
     text = Column(String(200))
+
+    answer = relationship('Answer', backref='quest')
 
 
 class Faculty(Base):
@@ -63,6 +81,9 @@ class Faculty(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50))
     url = Column(String(200))
+
+    answer_faculty = relationship("AnswerFaculty", backref="faculty")
+    result_faculty = relationship("ResultFaculty", backref="faculty")
 
 
 Base.metadata.create_all(bind=engine)
