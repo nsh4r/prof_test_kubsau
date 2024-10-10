eabf4f39b6ff615cf28c0355d3c245408f0ed365
Sharapatov Nikita <neffshar@gmail.com>
2024-10-10 12:23:45 +0300

    edit result reques

--- prof_test_kubsau/server.py
+++ prof_test_kubsau/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, status
 from sqlmodel import Session, select
 
-from prof_test_kubsau.schemas import ResultInfo, ResponseResult
+from prof_test_kubsau.schemas import ResultInfo, ResponseResult, FacultyType
 from prof_test_kubsau.database import Result, create_db_and_tables, Faculty, ResultFaculty, FacultyType, engine
 
 app = FastAPI()
@@ -12,44 +12,61 @@ def on_startup():
     create_db_and_tables()
 
 
-@app.post('/api/test/result/', response_model=ResponseResult)  # response_model=ResponseResult
+@app.post('/api/test/result/', response_model=ResponseResult)
 def get_specific_result(request_result: ResultInfo):
     """Получает данные в теле запроса, производит поиск по номеру телефона.
     Если результат не найден, возвращает ошибку 404."""
 
+    # Валидируем модель запроса
     db_result = Result.model_validate(request_result)
-    faculties = []
+
+    faculties_list = []  # Создаем пустой список для хранения типов факультетов
+
     with Session(engine) as session:
+        # Поиск профиля пользователя по номеру телефона
         profile = (select(Result, ResultFaculty)
-                   .join(ResultFaculty).
-                   where(Result.phone_number == db_result.phone_number))    # ваыбираем данные исходя из номера
+                   .join(ResultFaculty)
+                   .where(Result.phone_number == db_result.phone_number))
         result = session.exec(profile).all()
-        print(result)
+
         if not result:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Result not found!')
-        faculty_type_ids = [item[1].faculty_type_id for item in result]     # получаем номер ледирующего типа факультета
-        print(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!{faculty_type_ids}')
-        for i in faculty_type_ids:
-            query_faculty = (select(Faculty).where(Faculty.type_id == i))   # ищем факультеты принадлежащие к типу
-            faculty = session.exec(query_faculty).first()
-            print(f'{faculty}')
-            faculties.append(faculty)
-            print(faculties)
-        if not faculties:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Faculties not found!')
-        for i in faculty_type_ids:
-            faculty_type = select(FacultyType).where(FacultyType.id == i)   # поиск типа
-        faculty_type_result = session.exec(faculty_type).first()
-        if not faculty_type_result:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Faculties type not found!')
 
-    response_result = ResponseResult(                                       # заполнение формы вывода
+        # Получаем ID типов факультетов
+        faculty_type_ids = [item[1].faculty_type_id for item in result]
+
+        for i in faculty_type_ids:
+            # Поиск информации о типе факультета
+            faculty_type_query = select(FacultyType).where(FacultyType.id == i)
+            faculty_type_result = session.exec(faculty_type_query).first()
+
+            if not faculty_type_result:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Faculties type not found!')
+
+            # Поиск факультетов, относящихся к этому типу
+            query_faculty = select(Faculty).where(Faculty.type_id == i)
+            faculties = session.exec(query_faculty).all()
+
+            if not faculties:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Faculties not found!')
+
+            # Создаем объект FacultyType
+            faculty_type_obj = FacultyType(
+                name=faculty_type_result.name,
+                compliance=faculty_type_result.compliance,
+                faculties=faculties
+            )
+
+            # Добавляем объект в список
+            faculties_list.append(faculty_type_obj)
+
+    # Создаем объект ResponseResult для ответа
+    response_result = ResponseResult(
         surname=result[0][0].surname,
         name=result[0][0].name,
         patronymic=result[0][0].patronymic,
         phone_number=result[0][0].phone_number,
-        faculty_type=faculty_type_result.name,
-        faculties=faculties,
-        compliance=result[0][1].compliance
+        faculty_type=faculties_list  # Используем собранный список
     )
+
     return response_result
