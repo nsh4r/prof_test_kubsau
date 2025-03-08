07b98208e8ee90a6a46df599987f6cd313b9b87c
Sharapatov Nikita <neffshar@gmail.com>
2025-03-09 00:30:46 +0300

    model_config = SettingsConfigDict(env_file='.env', extra='allow'

--- backend/src/config.py
+++ backend/src/config.py
@@ -13,5 +13,6 @@ class Settings(BaseSettings):
     def postgres_url(self):
         return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
 
+    model_config = SettingsConfigDict(env_file='.env', extra='allow')
 
 settings = Settings()
