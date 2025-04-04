5588cf5d064658df17f786818a2c4089b2cf8c68
Sharapatov Nikita <neffshar@gmail.com>
2025-04-05 00:08:10 +0300

    add unit test to the project

--- backend/src/tests/utils.py
+++ backend/src/tests/utils.py
@@ -1,11 +1,12 @@
-from typing import Final
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from backend.src.config import settings
 
-
-Session = scoped_session(sessionmaker())
+async_engine = create_async_engine(settings.postgres_url)
+async_session = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
 
 
 class TestConstants:
-    """Константные значения для тестов"""
-
-    PHONE: Final[str] = '79000000000'
+    """Test constants"""
+    PHONE: str = '79000000000'
+    UUID: str = 'a1b2c3d4-e5f6-7890-1234-56789abcdef0'
