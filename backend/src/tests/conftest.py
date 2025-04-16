82fa22d1cf0448a04c7090c1cc4ccb3e4709df43
Sharapatov Nikita <neffshar@gmail.com>
2025-04-16 12:46:50 +0300

    fix async session

--- backend/src/tests/conftest.py
+++ backend/src/tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
 from backend.src.config import settings
