f5b40df8a7b69b906f47c573eed45b8afe4fc848
Sharapatov Nikita <neffshar@gmail.com>
2025-06-08 13:03:54 +0300

    allowedHosts: true,

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,10 +5,9 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
     port: 5173,
     strictPort: true,
-    allowedHosts: ['*'],
+    allowedHosts: true,
   },
   resolve: {
     alias: {
