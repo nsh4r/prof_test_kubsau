c7c11a8b0787499f4943a66ca1e27b76b81b2957
Sharapatov Nikita <neffshar@gmail.com>
2025-06-08 12:24:00 +0300

    update for docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['*']
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['*'],
   },
   resolve: {
     alias: {
