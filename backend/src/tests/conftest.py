0da77abef1a1e76cd7483048bf2ade16b0f58b2d
Sharapatov Nikita <neffshar@gmail.com>
2025-06-18 14:42:24 +0300

    update tests

--- backend/src/tests/conftest.py
+++ backend/src/tests/conftest.py
@@ -50,13 +50,25 @@ async def client(prepare_database):
 @pytest.fixture()
 async def test_data():
     async with TestSessionLocal() as session:
-        # Типы факультетов
-        faculty_type1 = FacultyType(uuid=UUID("11111111-1111-1111-1111-111111111111"), name="Технический")
-        faculty_type2 = FacultyType(uuid=UUID("22222222-2222-2222-2222-222222222222"), name="Гуманитарный")
+        # Сначала создаем экзамены
+        exam1 = Exam(uuid=UUID("236e43f1-6d9a-42d2-bf80-514e7ed3030c"),
+                    name="Русский язык",
+                    code="rus")
+        exam2 = Exam(uuid=UUID("bde589f5-c13e-4606-ad55-c394038091b8"),
+                    name="Математика (базовая)",
+                    code="math_basic")
+        session.add_all([exam1, exam2])
+        await session.commit()
+
+        # Затем создаем типы факультетов
+        faculty_type1 = FacultyType(uuid=UUID("11111111-1111-1111-1111-111111111111"),
+                                  name="Технический")
+        faculty_type2 = FacultyType(uuid=UUID("22222222-2222-2222-2222-222222222222"),
+                                  name="Гуманитарный")
         session.add_all([faculty_type1, faculty_type2])
         await session.commit()
 
-        # Факультеты
+        # Создаем факультеты
         faculty1 = Faculty(
             uuid=UUID("33333333-3333-3333-3333-333333333333"),
             name="Факультет информатики",
@@ -66,32 +78,37 @@ async def test_data():
         session.add(faculty1)
         await session.commit()
 
-        # Требования факультета к экзаменам
+        # Теперь можно создавать требования факультета к экзаменам
         exam_requirement = FacultyExamRequirement(
             faculty_id=faculty1.uuid,
-            exam_id=UUID("236e43f1-6d9a-42d2-bf80-514e7ed3030c"),
+            exam_id=exam1.uuid,
             min_score=60
         )
         session.add(exam_requirement)
         await session.commit()
 
-        # Остальные данные (вопросы, ответы и т.д.) остаются как были
-        question = Question(uuid=UUID("11111111-1111-1111-1111-111111111111"), text="Тестовый вопрос")
+        # Создаем вопрос
+        question = Question(uuid=UUID("11111111-1111-1111-1111-111111111111"),
+                          text="Тестовый вопрос")
         session.add(question)
         await session.commit()
 
-        answer1 = Answer(uuid=UUID("22222222-2222-2222-2222-222222222222"), text="Тестовый ответ 1", question_id=question.uuid)
-        answer2 = Answer(uuid=UUID("33333333-3333-3333-3333-333333333333"), text="Тестовый ответ 2", question_id=question.uuid)
+        # Создаем ответы
+        answer1 = Answer(uuid=UUID("22222222-2222-2222-2222-222222222222"),
+                        text="Тестовый ответ 1",
+                        question_id=question.uuid)
+        answer2 = Answer(uuid=UUID("33333333-3333-3333-3333-333333333333"),
+                        text="Тестовый ответ 2",
+                        question_id=question.uuid)
         session.add_all([answer1, answer2])
         await session.commit()
 
-        answer_faculty1 = AnswerFaculty(answer_id=answer1.uuid, faculty_type_id=faculty_type1.uuid, score=10)
-        answer_faculty2 = AnswerFaculty(answer_id=answer2.uuid, faculty_type_id=faculty_type2.uuid, score=5)
+        # Создаем связи ответов с факультетами
+        answer_faculty1 = AnswerFaculty(answer_id=answer1.uuid,
+                                      faculty_type_id=faculty_type1.uuid,
+                                      score=10)
+        answer_faculty2 = AnswerFaculty(answer_id=answer2.uuid,
+                                      faculty_type_id=faculty_type2.uuid,
+                                      score=5)
         session.add_all([answer_faculty1, answer_faculty2])
-        await session.commit()
-
-        # Экзамены
-        exam1 = Exam(uuid=UUID("236e43f1-6d9a-42d2-bf80-514e7ed3030c"), name="Русский язык", code="rus")
-        exam2 = Exam(uuid=UUID("bde589f5-c13e-4606-ad55-c394038091b8"), name="Математика (базовая)", code="math_basic")
-        session.add_all([exam1, exam2])
         await session.commit()
